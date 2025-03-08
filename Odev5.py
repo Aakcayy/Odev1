b8dfbf78492d248017e2ef5fd23c9289113c0e5c
Ahmet Özdemir Akçay <124554775+Aakcayy@users.noreply.github.com>
2025-03-08 17:43:15 +0300

    Add files via upload

---
+++ Odev5.py
@@ -0,0 +1,47 @@
+# sayi=int(input("Bir sayi giriniz ->> "))
+
+# if sayi%2==0:
+#     print("Girdiğin sayı çift bir sayıdır")
+
+# else:
+#     print("Girdiğin sayı tek sayıdır.")
+
+
+# sinav=int(input("Sınav notunuzu giriniz ->> "))
+
+# if sinav>=90:
+#     print("Harf notunuz ->> A")
+
+# elif sinav>=80 and sinav<90:
+#     print("Harf notunuz ->> B")
+
+# elif sinav>=70 and sinav<80:
+#     print("Harf notunuz ->> C")
+
+# elif sinav>=60 and sinav<70:
+#     print("Harf notunuz ->> D")
+
+# elif sinav>=0 and sinav<60:
+#     print("Harf notunuz ->> F")
+
+# else:
+#     print("Yanlış not girişi yapılmıştır.")
+
+yas=int(input("Yaşınızı giriniz ->> "))
+
+if yas>=60:
+    print("Yaşlı")
+
+elif yas>=20 and yas<60:
+    print("Yetişkin")
+
+elif yas>=13 and yas<20:
+    print("Genç")
+
+elif yas>=0 and yas<13:
+    print("Çocuk")
+
+else:
+    print("Yanlış  giriş yapılmıştır.\nTekrar deneyiniz.")
+
+    
